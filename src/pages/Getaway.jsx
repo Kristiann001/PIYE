9a094eb13c4ba0bfe93106e5287fddebd2931c60
Kristian Koome <originalkristiann@gmail.com>
2025-03-26 15:06:43 +0300

    Update Getaway

--- src/pages/Getaway.jsx
+++ src/pages/Getaway.jsx
@@ -69,7 +69,7 @@ export default function Getaway() {
               <div className="flex justify-center">
                 <a
                   href="/booknow"
-                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+                  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-green-900"
                 >
                   Book Now
                 </a>
